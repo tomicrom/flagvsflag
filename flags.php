028b040532cd578772460f30fbb6dc7e111b024f
Vladimir Martemianov <vmartemianov@gmail.com>
2017-01-12 17:17:01 -0300

    agrego javscript void para evitar el salto al top

--- flags.php
+++ flags.php
@@ -19,12 +19,12 @@ if($_POST['flag']>0 and isset($_POST['flag']) and is_numeric($_POST['flag']))
 ?>
 <div class="five columns izq" id="flag1">
     <p><?php echo $data_flag1['country']; ?></p>
-    <a href="#" onclick="javscript:voteFlag(<?php echo $data_flag2['id']; ?>)"><img src="images/flags/<?php echo $data_flag1['file']; ?>"></a>
+    <a href="javascript:void(0);" onclick="javscript:voteFlag(<?php echo $data_flag2['id']; ?>)"><img src="images/flags/<?php echo $data_flag1['file']; ?>" border="1"></a>
 </div>
 <div class="two columns">
     <h4 id="vs">VS.</h4>
 </div>
 <div class="five columns der" id="flag2">
     <p><?php echo $data_flag2['country']; ?></p>
-    <a href="#" onclick="javscript:voteFlag(<?php echo $data_flag2['id']; ?>)" id="<?php echo $data_flag2['id']; ?>"><img src="images/flags/<?php echo $data_flag2['file']; ?>"></a>
+    <a href="javascript:void(0);" onclick="javscript:voteFlag(<?php echo $data_flag2['id']; ?>)" id="<?php echo $data_flag2['id']; ?>"><img src="images/flags/<?php echo $data_flag2['file']; ?>" style="border: 1px;"></a>
 </div>
